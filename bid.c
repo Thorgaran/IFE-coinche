484bcade339e84b79065b2b4be20457bed4f9030
Thorgaran <39826071+Thorgaran@users.noreply.github.com>
2020-05-09 13:52:28 +0200

    Add getPlayerContract function
    Decides who has to be asked between the User and the various AIs
    New placeholder askUserContract function

--- bid.c
+++ bid.c
@@ -3,3 +3,21 @@
 #include "core.h"
 #include "userIO.h"
 #include "ai.h"
+
+Bool getPlayerContract(Player player, Contract *contract) {
+    Bool hasPassed = TRUE;
+    switch (player.type) {
+        case USER: //If the player is the User
+            hasPassed = askUserContract(player.cards, player.nbOfCards, &(*contract));
+            break;
+        case AI_STANDARD: //If the player is an AI of type STANDARD
+            hasPassed = getAIContractStandard(player.cards, player.nbOfCards, &(*contract));
+            break;
+        default: //Default behaviour if this AI type has no dedicated contract function
+            hasPassed = getAIContractStandard(player.cards, player.nbOfCards, &(*contract));
+    }
+    if (hasPassed == FALSE) { //If the player decided to make a contract
+        (*contract).issuer = player.pos; //Set the contract issuer as being this player
+    }
+    return hasPassed;
+}
--- bid.h
+++ bid.h
@@ -2,6 +2,11 @@
 #define _BID_H_
 #include "core.h"
 
-
+/* Get the player to decide whether or not to make a contract, depending on the player type (User/AI)
+*   @param player: the player who has to make a decision
+*   @param *contract: pointer to the contract being debated. Will be edited if the player decides to make a contract
+*   @return hasPassed: TRUE if the player has decided to pass, FALSE if the player decided to make a contract
+*/
+Bool getPlayerContract(Player player, Contract *contract);
 
 #endif
