7743f5b7566e9ce2b5e3e48d35f0774dd981d94a
Ithamix <63819619+Ithamix@users.noreply.github.com>
2020-05-30 17:59:55 +0200

    Add displayTrick and updateTrickDisplay function

--- src/userIO.c
+++ src/userIO.c
@@ -11,3 +11,40 @@ Bool askUserContract(Card cardArray[], int nbOfCards, Contract *contract) {
     Bool hasPassed = TRUE;
     return hasPassed;
 }
+
+void displayTrick(Player* players,Color trump, int nbOfTheTrick, Contract contract, Card* playerHand){
+    
+    printf("Trick %d / 8\nTrump : %d",nbOfTheTrick,trump);
+    switch (contract.type){
+    case POINTS: printf("\n%d points %d contract in %d made by %d\n\n",contract.points,contract.coinche,contract.trump,contract.issuer);
+        break;
+    
+    default: printf("\n%d %d contract in %d made by %d\n\n",contract.type,contract.coinche,contract.trump,contract.issuer);
+        break;
+    }
+    printf("\t\t\t\tNorth\n\n\t\t\t\t \nWest\t \t\t\t\t \tEast\n\t\t\t\t \n\t\t\t   %s",players[0].name);
+    printf("\n\nYour hand : ");
+    for (int cardNb = 0; cardNb < 8; cardNb++){
+    printf("%d%d ",playerHand[cardNb].value,playerHand[cardNb].color);
+    }
+    
+}
+
+void updateTrickDisplay(Position playerPosition,Card playedCard, Card* playerHand, int nbOfCardsInHand){
+    switch (playerPosition)
+    {
+    case NORTH: printf("\e[5;18H");
+        break;
+    case SOUTH: printf("\e[10;18H");
+        break;
+    case WEST: printf("\e[8;10H");
+        break;
+    case EAST: printf("\e[8;24H");
+        break;
+    }
+    printf("%d%d",playedCard.value,playedCard.color);
+    printf("\e[13;13H");
+    for (int cardNb = 0; cardNb < nbOfCardsInHand; cardNb++){
+    printf("%d%d ",playerHand[cardNb].value,playerHand[cardNb].color);
+    }
+}
