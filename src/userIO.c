e5c70aeebfe7ff101fea45e7ad6ec34e09f53816
Thorgaran <39826071+Thorgaran@users.noreply.github.com>
2020-05-31 23:44:36 +0200

    Include header files in the respective c files
    Somehow we didn't run into an issue with this for pretty long

--- src/userIO.c
+++ src/userIO.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "core.h"
+#include "userIO.h"
 
 Card askUserCard(Card cardArray[], int nbOfCards) {
     Card chosenCard = {.color = SPADE, .value = SEVEN};
